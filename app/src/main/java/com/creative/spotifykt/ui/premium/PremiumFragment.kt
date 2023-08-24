c6c4c86dd38d4c746bc47e2d3d3e699da9f18337
Dan Tech <danhtran.dev@outlook.com>
2023-08-24 22:19:14 +0800

    Premium tab

--- app/src/main/java/com/creative/spotifykt/ui/premium/PremiumFragment.kt
+++ app/src/main/java/com/creative/spotifykt/ui/premium/PremiumFragment.kt
@@ -3,16 +3,11 @@ package com.creative.spotifykt.ui.premium
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.creative.spotifykt.data.model.local.PremiumItemData
+import com.creative.spotifykt.core.ui.BaseFragment
 import com.creative.spotifykt.databinding.PremiumFragmentBinding
 import com.creative.spotifykt.di.component.FragmentComponent
-import com.creative.spotifykt.core.ui.BaseFragment
 
 class PremiumFragment : BaseFragment<PremiumFragmentBinding, PremiumViewModel>() {
-    private lateinit var recyclerPremiumInfo: RecyclerView
-    private val infoAdapter: PremiumInfoAdapter by lazy { PremiumInfoAdapter() }
 
     override fun provideViewBinding(inflater: LayoutInflater, container: ViewGroup?): PremiumFragmentBinding =
         PremiumFragmentBinding.inflate(inflater, container, false)
@@ -22,19 +17,8 @@ class PremiumFragment : BaseFragment<PremiumFragmentBinding, PremiumViewModel>()
     }
 
     override fun setupView(savedInstanceState: Bundle?) {
-        recyclerPremiumInfo = requireViewBinding().listDiffInfo
+        viewBinding?.apply {
 
-        recyclerPremiumInfo.adapter = infoAdapter
-        recyclerPremiumInfo.layoutManager = LinearLayoutManager(context).apply {
-            orientation = LinearLayoutManager.HORIZONTAL
         }
-
-        infoAdapter.differ.submitList(mutableListOf<PremiumItemData?>().apply {
-            add(PremiumItemData("Add breaks", "Ad free music"))
-            add(PremiumItemData("Play in shuffle", "Play any song"))
-            add(PremiumItemData("6 skips per hour", "Unlimited skips"))
-            add(PremiumItemData("Streaming only", "Offline listening"))
-            add(PremiumItemData("Basic quality", "High audio quality"))
-        })
     }
 }
--- app/src/main/java/com/creative/spotifykt/ui/premium/PremiumViewModel.kt
+++ app/src/main/java/com/creative/spotifykt/ui/premium/PremiumViewModel.kt
@@ -2,4 +2,6 @@ package com.creative.spotifykt.ui.premium
 
 import com.creative.spotifykt.core.viewmodel.BaseViewModel
 
-class PremiumViewModel : BaseViewModel()
+class PremiumViewModel : BaseViewModel() {
+
+}
