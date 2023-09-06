f4403a4663541d68835090c85da2584f1530c27d
Dan Tech <danhtran.dev@outlook.com>
2023-09-07 00:22:07 +0700

    setup other setting about screens

--- app/src/main/java/com/creative/spotifykt/ui/setting/about/AboutFragment.kt
+++ app/src/main/java/com/creative/spotifykt/ui/setting/about/AboutFragment.kt
@@ -2,16 +2,18 @@ package com.creative.spotifykt.ui.setting.about
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.creative.spotifykt.databinding.AboutFragmentBinding
-import com.creative.spotifykt.databinding.LayoutToolbarBinding
-import com.creative.spotifykt.di.component.FragmentComponent
+import com.creative.spotifykt.R
 import com.creative.spotifykt.core.ui.BaseFragment
+import com.creative.spotifykt.data.model.local.AppBarUI
+import com.creative.spotifykt.data.model.local.ColorStyle
+import com.creative.spotifykt.data.model.local.TextLabel
+import com.creative.spotifykt.databinding.AboutFragmentBinding
+import com.creative.spotifykt.di.component.FragmentComponent
+import com.creative.spotifykt.ui.IAppBarHandler
 
-class AboutFragment : BaseFragment<AboutFragmentBinding, AboutViewModel>(), View.OnClickListener {
-    private lateinit var settingToolbarBinding: LayoutToolbarBinding
+class AboutFragment : BaseFragment<AboutFragmentBinding, AboutViewModel>() {
     override fun provideViewBinding(inflater: LayoutInflater, container: ViewGroup?): AboutFragmentBinding =
         AboutFragmentBinding.inflate(inflater, container, false)
 
@@ -20,14 +22,12 @@ class AboutFragment : BaseFragment<AboutFragmentBinding, AboutViewModel>(), View
     }
 
     override fun setupView(savedInstanceState: Bundle?) {
-        settingToolbarBinding = requireViewBinding().settingToolbar
-        settingToolbarBinding.settingBackNav.setOnClickListener(this)
-    }
-
-    override fun onClick(v: View?) {
-        when (v!!) {
-            settingToolbarBinding.settingBackNav -> {
-                findNavController().navigateUp()
+        viewBinding?.apply {
+            settingToolbar.data = AppBarUI(title = TextLabel(getString(R.string.about), ColorStyle.PRIMARY.value))
+            settingToolbar.appBarHandler = object : IAppBarHandler {
+                override fun handleBack() {
+                    findNavController().navigateUp()
+                }
             }
         }
     }
